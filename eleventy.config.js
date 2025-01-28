c78de5f9ea135904e06195ee7502733ba637fad6
matejvitspselit <vitma1@spselit.cz>
2025-01-28 09:10:30 +0100

    Add files via upload

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+export default async function (eleventyConfig) {
+    //configure eleventy
+    eleventyConfig.setInputDirectory("./src");
+    eleventyConfig.setOutputDirectory("./dist");
+    eleventyConfig.setIncludesDirectory("templates");
+  
+    eleventyConfig.setTemplateFormats("html,njk");
+  
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("css");
+  
+    return {
+      //define what engines will be used for which files
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      dataTemplateEngine: "njk",
+    };
+  }
+  
